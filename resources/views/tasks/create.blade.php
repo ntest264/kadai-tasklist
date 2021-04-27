7013ba749edde988de6b896a4367d8524540c54d
n.test <testn960@gmail.com>
2021-04-27 09:20:12 +0000

    CRUD tasklist3

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
 
-<!-- ここにページ毎のコンテンツを書く -->
+ <h1>タスク新規作成ページ</h1>
+
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
+
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+
+ public function create()
+    {
+        $tasks = new Tasks;
+
+        // メッセージ作成ビューを表示
+        return view('tasks.create', [
+            'tasks' => $tasks,
+        ]);
+    }
 
 @endsection
