da6542fcd87de1907bc5d1b7db5b4516d3641d04
n.test <testn960@gmail.com>
2021-04-27 06:40:38 +0000

    CRUD tasklist

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+
+Route::get('tasks/{id}', 'TasksController@show');
+Route::post('tasks', 'TasksController@store');
+Route::put('tasks/{id}', 'TasksController@update');
+Route::delete('tasks/{id}', 'TasksController@destroy');
+
+Route::get('tasks', 'TasksController@index')->name('tasks.index');
+Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
+Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
